042478ea7ec3094f1f05ce9a8ab22ec38e8bb0d8
Ononobi Praise <ononobip@gmail.com>
2025-03-07 20:13:58 +0100

    fix: UI update for when file is shared

--- src/pages/d/[slug].tsx
+++ src/pages/d/[slug].tsx
@@ -46,15 +46,15 @@ const Redirect = () => {
 
     return (
         <div className='w-full items-center justify-center h-screen flex'>
-            <div className='w-[300px] mx-auto p-4 rounded-lg shadow-blue-500 flex items-center justify-between gap-3 shadow-sm h-[300px]'>
-                <div className='w-[100px] h-[200px] relative'>
+            <div className='w-[300px] mx-auto p-4 rounded-lg shadow-blue-500 flex flex-col items-center justify-between gap-3 shadow-sm min-h-[300px]'>
+                <div className='w-full p-4 h-[200px] relative'>
                     <Image src={`https://claraanalytics.com/wp-content/uploads/2023/01/Contribute.png`} alt={selectedFile[0]?.data?.fileName} layout='fill' objectFit='cover' />
                 </div>
-                <div className='w-full text-left text-base flex flex-col items-start justify-start font-bold gap-3 text-black'>
+                <div className='w-full text-left text-base flex flex-col items-center justify-center font-bold gap-3 text-black'>
                     <p className='w-full text-sm text-blue-600'>{selectedFile[0]?.data?.fileName}</p>
                     <p className='w-full text-sm text-blue-600'>By {selectedFile[0]?.data?.userName} <br /></p>
                 </div>
-                <button className='w-15 h-15 p-2 bg-blue-600 text-white rounded-full' onClick={() => handleDownload(selectedFile[0]?.data?.fileURL, selectedFile[0]?.data?.fileName)}>
+                <button className='w-full p-2 bg-blue-600 text-white rounded-lg text-center' onClick={() => handleDownload(selectedFile[0]?.data?.fileURL, selectedFile[0]?.data?.fileName)}>
                     <BiDownload size={20} />
                 </button>
             </div>
